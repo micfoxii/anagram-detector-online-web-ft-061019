e5fc8af1b217bf17907f365ad26b4ea887cba13b
Michael Fox <michael.c.fox.ii@gmail.com>
2019-06-18 18:34:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(word_array)
+    word_array.find_all do |w|
+      (@word.split("").sort) == (w.split("").sort)
+    end
+  end
+  
+end
 # Your code goes here!
